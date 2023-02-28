6f1f4607663fa79bee14b98e626228bc8f814f70
shiftgithub <mhturanmohammad@gmail.com>
2023-03-01 00:09:12 +0600

    work in app.js + app.css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="center-column">
+        <div className="item-row">
+          <span>One</span>
+        </div>
+        <div className="item-row">
+          <span>Two</span>
+        </div>
+        <div className="item-row">
+          <span>Three</span>
+        </div>
+        <div>
+        {/* <form>
+          <label>
+            Name:
+            <input type="text" name="name" />
+          </label>
+          <input type="submit" value="Submit" />
+        </form> */}
+      </div>
+      </div>
+      
     </div>
   );
 }
